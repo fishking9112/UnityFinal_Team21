584e012ce6dee3c1a9ee54048699ec5357ebdb47
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-04-07 20:22:24 +0900

    [Feat] Queen 구조 구현
    Queen이 관련된 스크립트를 모두 알고 있고, GameManager가 Queen을 알고 있도록 함

---
+++ Assets/01.Scripts/03.Player/QueenController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class QueenController : MonoBehaviour
+{
+    private Queen queen;
+
+    private void Awake()
+    {
+        //player = GameManager.Instance.queen; 
+    }
+}
