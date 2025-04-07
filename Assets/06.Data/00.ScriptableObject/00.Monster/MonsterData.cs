333ade2ed11578731ed2739ae9e62a9fbda9cc89
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-04-07 22:59:30 +0900

    [Fix] MonsterData 수정
    몬스터가 필요한 정보를 가져올 수 있도록 수정

--- Assets/06.Data/00.ScriptableObject/00.Monster/MonsterData.cs
+++ Assets/06.Data/00.ScriptableObject/00.Monster/MonsterData.cs
@@ -5,10 +5,30 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 
+public enum MonsterType
+{
+    MELEE,
+    RANGED,
+}
+
 [Serializable]
 public class MonsterInfo
 {
     public int id;
+    public string name;
+    public string description;
+    public float health;
+    public float defence;
+    public float cost;
+    public float moveSpeed;
+    public float attack;
+    public float attackRange;
+    public float attackSpeed;
+    public float reward;
+    public string outfit;
+
+    public MonsterType type;
+    public string projectile;
 }
 
 [CreateAssetMenu(fileName ="MonsterData", menuName ="Scriptable Object/New MonsterData")]
@@ -28,6 +48,48 @@ public class MonsterData : SheetDataReaderBase
             {
                 case "id":
                     monsterInfo.id = int.Parse(cell.value);
+                    break;
+                case "name":
+                    monsterInfo.name = cell.value;
+                    break;
+                case "description":
+                    monsterInfo.description = cell.value;
+                    break;
+                case "health":
+                    monsterInfo.health = float.Parse(cell.value);
+                    break;
+                case "defence":
+                    monsterInfo.defence = float.Parse(cell.value);
+                    break;
+                case "cost":
+                    monsterInfo.cost = float.Parse(cell.value);
+                    break;
+                case "moveSpeed":
+                    monsterInfo.moveSpeed = float.Parse(cell.value);
+                    break;
+                case "attack":
+                    monsterInfo.attack = float.Parse(cell.value);
+                    break;
+                case "attackRange":
+                    monsterInfo.attackRange = float.Parse(cell.value);
+                    break;
+                case "attackSpeed":
+                    monsterInfo.attackSpeed = float.Parse(cell.value);
+                    break;
+                case "reward":
+                    monsterInfo.reward = float.Parse(cell.value);
+                    break;
+                case "outfit":
+                    monsterInfo.outfit = cell.value;
+                    break;
+                case "type":
+                    monsterInfo.type = (MonsterType)Enum.Parse(typeof(MonsterType), cell.value);
+                    break;
+                case "projectile":
+                    if(monsterInfo.type == MonsterType.RANGED)
+                    {
+                        monsterInfo.projectile = cell.value;
+                    }
                     break;
             }
         }
