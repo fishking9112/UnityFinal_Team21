e11f03b660a90f4f4307da17391176cd2e2d0baa
박진우 <tree6316mte@gmail.com>
2025-04-10 10:47:03 +0900

    [Feat] Title Scene UI 러프(1차)

---
+++ Assets/01.Scripts/01.UI/MainUI/MainUI.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[Serializable]
+public class MainUIButtonPanel
+{
+    public string name;
+    public Button button;
+    public GameObject panel;
+}
+
+
+public class MainUI : MonoBehaviour
+{
+    public GameObject buttonMenu;
+    public List<MainUIButtonPanel> mainUISets;
+
+    public GameObject uiMenu;
+    public Button backBtn;
+
+    public GameObject goldPannel;
+    public GameObject upgradePointPannel;
+
+    private GameObject goActive;
+
+
+    void Start()
+    {
+        foreach (var mainUISet in mainUISets)
+        {
+            mainUISet.button.onClick.AddListener(() =>
+            {
+                mainUISet.panel.SetActive(true);
+                buttonMenu.SetActive(false);
+                uiMenu.SetActive(true);
+                goActive = mainUISet.panel;
+
+                if (mainUISet.panel.name == "MonsterUpgradeUI") // 추후 수정 필요
+                {
+                    goldPannel.SetActive(false);
+                    upgradePointPannel.SetActive(true);
+                }
+            });
+
+            backBtn.onClick.AddListener(() =>
+            {
+                goActive.SetActive(false);
+                buttonMenu.SetActive(true);
+                uiMenu.SetActive(false);
+
+                if (goActive.name == "MonsterUpgradeUI") // 추후 수정 필요
+                {
+                    goldPannel.SetActive(true);
+                    upgradePointPannel.SetActive(false);
+                }
+            });
+        }
+    }
+
+}
