3433a1fdf4f0729c7dd7eef7e6abb87c53be7157
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-04-16 11:08:15 +0900

    [Feat] 진화 트리 UI 구성

--- Assets/01.Scripts/01.UI/00.InGame/IngameUI.cs
+++ Assets/01.Scripts/01.UI/00.InGame/IngameUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class IngameUI : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class IngameUI : MonoBehaviour
     [Header("타이머")]
     [SerializeField] private TimerUI timerUI;
     [SerializeField] private float limitTime = 1800f;
+
+    [Header("진화 트리")]
+    [SerializeField] private GameObject evolutionTreeWindow;
 
     private ReactiveProperty<float> CurTime = new ReactiveProperty<float>();
     private QueenCondition condition;
@@ -58,5 +62,13 @@ public class IngameUI : MonoBehaviour
     private void Clear()
     {
         // 제한 시간 동안 버텼을 경우
+    }
+
+    public void OnEvolutionWindow(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Started)
+        {
+            evolutionTreeWindow.SetActive(!evolutionTreeWindow.activeInHierarchy);
+        }
     }
 }
