38ea8b47e3e2b697b39a109e5c284b75a4b8b46e
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-04-17 15:28:18 +0900

    [Feat] 진화 트리 각 노드에 대한 스크립트 작성

---
+++ Assets/01.Scripts/01.UI/00.InGame/00.EvolutionTree/EvolutionNode.cs
@@ -0,0 +1,79 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EvolutionNode : MonoBehaviour
+{
+    [Header("UI")]
+    public Image image;
+    [SerializeField] private Button button;
+    [SerializeField] private TextMeshProUGUI nameText;
+
+    public bool isUnlock;
+    public bool nodeLock;
+    public int monsterInfoId;
+    public MonsterInfo monsterInfo;
+
+    private EvolutionTree evolutionTree;
+
+    public void Init(EvolutionTree tree)
+    {
+        evolutionTree = tree;
+
+        if(DataManager.Instance.monsterDic.TryGetValue(monsterInfoId,out var info))
+        {
+            image.sprite = DataManager.Instance.iconData.GetSprite(monsterInfo.outfit);
+            monsterInfo = info;
+        }
+        else
+        {
+            Utils.Log($"{monsterInfoId}의 info는 존재하지 않습니다.");
+        }
+
+        button.onClick.AddListener(OnClickNode);
+        UpdateButtonState();
+    }
+
+    public void OnClickNode()
+    {
+        evolutionTree.OnClickNodeButton(this);
+    }
+
+    public void UpdateButtonState()
+    {
+        if (nodeLock)
+        {
+            button.interactable = false;
+            image.color = Color.red;
+            nameText.text = "잠김";
+            return;
+        }
+
+        bool isActive = evolutionTree.ActiveCheck(this);
+
+        // 해금 된 노드
+        if (isUnlock)
+        {
+            image.color = Color.green;
+            nameText.text = monsterInfo.name;
+            button.interactable = true;
+        }
+        else
+        {
+            // 해금 안된 것 중 해금할 수 있는 노드
+            if (isActive)
+            {
+                image.color = Color.blue;
+                nameText.text = monsterInfo.name;
+                button.interactable = true;
+            }
+            // 아직 해금할 수 없는 노드
+            else
+            {
+                image.color = Color.black;
+                nameText.text = "???";
+                button.interactable = false;
+            }
+        }
+    }
+}
