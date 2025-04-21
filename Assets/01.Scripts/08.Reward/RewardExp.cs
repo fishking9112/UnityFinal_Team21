e4dc417d3bf6a2faa028fed901ce47d317c25188
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-04-21 19:18:22 +0900

    [Feat] 골드 보상 아이템 구현

--- Assets/01.Scripts/08.Reward/RewardExp.cs
+++ Assets/01.Scripts/08.Reward/RewardExp.cs
@@ -1,10 +1,7 @@
-using UnityEngine;
-
 public class RewardExp : RewardBase
 {
     protected override void GainReward()
     {
         condition.AdjustCurExpGauge(rewardAmount);
-        Debug.Log(condition.CurExpGauge.Value);
     }
 }
