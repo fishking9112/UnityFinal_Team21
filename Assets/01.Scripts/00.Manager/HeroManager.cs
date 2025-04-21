2676ec0fedd363b95e8ee5d201c0cd14ef298d67
jj930220 <jj930220@gmail.com>
2025-04-21 16:01:15 +0900

    [Feat] 히어로 소환 알고리즘 작성중

--- Assets/01.Scripts/00.Manager/HeroManager.cs
+++ Assets/01.Scripts/00.Manager/HeroManager.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -18,32 +19,57 @@ public class HeroManager : MonoSingleton<HeroManager>
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.L))
+        //if(Input.GetKeyDown(KeyCode.L))
+        //{
+        //    rand = UnityEngine.Random.Range(-45, 45);
+        //    rand2 = UnityEngine.Random.Range(-45, 45);
+        //    idList.Clear();
+        //    levelList.Clear();
+        //    //var l = Enumerable.Range(101, 5).OrderBy(x => UnityEngine.Random.value).Take(3);
+
+        //    //foreach(int i in l)
+        //    //{
+        //    //    idList.Add(i);
+        //    //    Utils.Log(i.ToString());
+        //    //}
+
+        //    //var lv= Enumerable.Range(1,8).OrderBy(x => UnityEngine.Random.value).Take(3);
+
+        //    //foreach (int i in lv)
+        //    //{
+        //    //    levelList.Add(i);
+        //    //    Utils.Log(i.ToString());
+        //    //}
+        //    HeroController hero = HeroPoolManager.Instance.GetObject(new Vector2(rand,rand2));
+        //    hero.InitAbility(idList, levelList);
+        //}
+    }
+
+    private async UniTaskVoid SetWave()
+    {
+        while(true) // 게임 끝나기 전까지
         {
-            rand = UnityEngine.Random.Range(-45, 45);
-            rand2 = UnityEngine.Random.Range(-45, 45);
-            idList.Clear();
-            levelList.Clear();
-            //var l = Enumerable.Range(101, 5).OrderBy(x => UnityEngine.Random.value).Take(3);
+            await UniTask.Delay(TimeSpan.FromSeconds(3)); //매 3초마다
 
-            //foreach(int i in l)
-            //{
-            //    idList.Add(i);
-            //    Utils.Log(i.ToString());
-            //}
-
-            //var lv= Enumerable.Range(1,8).OrderBy(x => UnityEngine.Random.value).Take(3);
-
-            //foreach (int i in lv)
-            //{
-            //    levelList.Add(i);
-            //    Utils.Log(i.ToString());
-            //}
-            idList.Add(102);
-            levelList.Add(1);
-
-            HeroController hero = HeroPoolManager.Instance.GetObject(new Vector2(rand,rand2));
-            hero.InitAbility(idList, levelList);
+            SetNextHero();
+            SummonHero();
         }
     }
+
+    private void SetNextHero()
+    {
+        idList.Clear();
+        levelList.Clear();
+
+        idList.Add(102);
+        levelList.Add(1);
+    }
+
+
+    private void SummonHero()
+    {
+        HeroController hero = HeroPoolManager.Instance.GetObject(new Vector2(rand, rand2));
+        hero.InitAbility(idList, levelList);
+    }
+
 }
