ec74b8a7b04f34d76f1617924f9b9f67dcd52162
박진우 <tree6316mte@gmail.com>
2025-04-22 16:11:26 +0900

    [Fix] Monster 네비메시 Poisiton업데이트 오류 수정

--- Assets/01.Scripts/04.Monster/MonsterState/MonsterTrackingState.cs
+++ Assets/01.Scripts/04.Monster/MonsterState/MonsterTrackingState.cs
@@ -89,18 +89,18 @@ public class MonsterTrackingState : MonsterBaseState
         // 타겟이 없다면 움직임 없음 (0.1초마다 반복되게 여기서 return)
         if (target == null)
         {
-            if (navMeshAgent.updatePosition)
+            if (navMeshAgent.enabled)
             {
                 spum.PlayAnimation(PlayerState.IDLE, 0);
-                navMeshAgent.updatePosition = false;
+                navMeshAgent.enabled = false;
             }
             return;
         }
 
         // 움직임 시작
-        if (!navMeshAgent.updatePosition)
+        if (!navMeshAgent.enabled)
         {
-            navMeshAgent.updatePosition = true;
+            navMeshAgent.enabled = true;
         }
 
         // 움직임-----
