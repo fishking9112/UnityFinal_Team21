42194ed11040ebaee71b32a1932fc07a9bfef0d1
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-05-13 20:28:28 +0900

    [Feat] WarcryBuffSkill 구현

---
+++ Assets/01.Scripts/03.Queen/00.QueenActiveSkill/WarcryBuffSkill.cs
@@ -0,0 +1,32 @@
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarcryBuffSkill : QueenActiveSkillBase
+{
+    public override void Init()
+    {
+        base.Init();
+
+        info = DataManager.Instance.queenActiveSkillDic[(int)IDQueenActiveSkill.WARCRY];
+    }
+
+    public override async void UseSkill()
+    {
+        // 마우스 위치를 기준으로 size만큼 충돌 검사
+        Vector3 mousePos = controller.worldMousePos;
+        Collider2D[] hits = Physics2D.OverlapCircleAll(mousePos, info.size, info.target);
+
+        // 충돌한 모든 몬스터에게 버프 적용
+        List<UniTask> tasks = new List<UniTask>();
+        foreach (var hit in hits)
+        {
+            if (MonsterManager.Instance.monsters.TryGetValue(hit.gameObject, out var monster))
+            {
+                UniTask warcryTask = BuffManager.Instance.ApplyBuff(monster, info.buff_ID, info.buff_Level);
+                tasks.Add(warcryTask);
+            }
+        }
+        await UniTask.WhenAll(tasks);
+    }
+}
