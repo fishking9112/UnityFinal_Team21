9834c4c91e29e77be275f77860e5fa0ca4e70588
dalsi <tkdwns8632@gmail.com>
2025-05-14 21:07:49 +0900

    [Chore] 불필요 코드 삭제

--- Assets/01.Scripts/01.UI/UIManager/Single/PauseUI.cs
+++ Assets/01.Scripts/01.UI/UIManager/Single/PauseUI.cs
@@ -3,22 +3,15 @@ using UnityEngine.UI;
 
 public class PauseUI : SingleUI
 {
-    [Header("패널들")]
-    public GameObject optionPanel;
+    [Header("UIs")]
     [SerializeField] private QueenEnhanceStatusUI queenEnhanceStatusUI;
-
-    [Header("UI 버튼들")]
-    public RectTransform cameraRect;
-
-    [Header("UI 버튼들")]
-    public Button continueButton;
-    public Button optionButton;
     public Button exitButton;
+
+
+    public RectTransform cameraRect;
 
     private void Awake()
     {
-        continueButton.onClick.AddListener(CloseUI);
-        optionButton.onClick.AddListener(OnClickOption);
         exitButton.onClick.AddListener(OnClickGameResult);
     }
 
@@ -28,35 +21,11 @@ public class PauseUI : SingleUI
     }
 
     /// <summary>
-    /// 옵션 패널 활성화/비활성화
-    /// </summary>
-    public void SetOptionPanel(bool active)
-    {
-        optionPanel?.SetActive(active);
-    }
-
-    /// <summary>
     /// 강화 상태 새로고침
     /// </summary>
     public void RefreshStatus()
     {
         queenEnhanceStatusUI.RefreshStatus();
-    }
-
-    /// <summary>
-    /// 일시정지 창 닫기
-    /// </summary>
-    private void CloseUI()
-    {
-        StaticUIManager.Instance.hudLayer.GetHUD<GameHUD>().HideWindow();
-    }
-
-    /// <summary>
-    /// 옵션 버튼 눌렀을 때
-    /// </summary>
-    private void OnClickOption()
-    {
-        SetOptionPanel(true);
     }
 
     /// <summary>
