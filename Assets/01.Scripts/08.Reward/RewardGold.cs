daf4e1811b3521dc370331b0ad206cb525a14542
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-05-18 17:27:38 +0900

    [Fix] 보상 획득 방식 수정
    보상이 마우스 커서에 닿으면 경험치는 레벨UI쪽으로 골드는 골드UI쪽으로 끌려가도록 수정

--- Assets/01.Scripts/08.Reward/RewardGold.cs
+++ Assets/01.Scripts/08.Reward/RewardGold.cs
@@ -1,5 +1,14 @@
+using System;
+using UnityEngine;
+
 public class RewardGold : RewardBase
 {
+    public override void Init(Action<Component> returnAction)
+    {
+        base.Init(returnAction);
+        SetMagnetTarget(RewardManager.Instance.goldTarget);
+    }
+
     protected override void GainReward()
     {
         condition.AdjustGold(rewardAmount);
