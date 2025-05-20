5b51af321de4a210fdc69f772c6dbd6072511c9c
박진우 <tree6316mte@gmail.com>
2025-05-20 21:06:45 +0900

    [Feat] 유닛 소환 성 내에만 가능하게 하기

--- Assets/01.Scripts/05.Controller/SpawnPointController.cs
+++ Assets/01.Scripts/05.Controller/SpawnPointController.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class SpawnPointController : MonoBehaviour
 {
-    [SerializeField]
-    private List<Rect> spawnAreas; // 적을 생성할 영역 리스트
+    [SerializeField] private List<Rect> spawnAreas; // 적을 생성할 영역 리스트
+    [SerializeField] private Color gizmoColor = new Color(1, 0, 0, 0.3f); // 기즈모 색상
 
-    [SerializeField]
-    private Color gizmoColor = new Color(1, 0, 0, 0.3f); // 기즈모 색상
+    private List<SpriteRenderer> visualizers = new List<SpriteRenderer>();
+
+
+    private void Start()
+    {
+        CreateVisualizers();
+    }
+
+
+    private void CreateVisualizers()
+    {
+        foreach (var area in spawnAreas)
+        {
+            SpriteRenderer sr = Instantiate(SpawnPointManager.Instance.areaVisualizerPrefab, transform);
+            sr.gameObject.transform.position = new Vector3(area.x + area.width / 2, area.y + area.height / 2, 0);
+            sr.gameObject.transform.localScale = new Vector3(area.width, area.height, 1);
+            sr.color = gizmoColor;
+            sr.enabled = false; // 처음엔 비활성화
+
+            visualizers.Add(sr);
+        }
+    }
 
     public Vector2 GetRandomPosition(bool isCenter = false)
     {
@@ -45,6 +65,54 @@ public class SpawnPointController : MonoBehaviour
             Vector3 center = new Vector3(area.x + area.width / 2, area.y + area.height / 2);
             Vector3 size = new Vector3(area.width, area.height);
             Gizmos.DrawCube(center, size);
+        }
+    }
+    public bool IsAreaIn(Vector2 areaPoint)
+    {
+        foreach (var area in spawnAreas)
+        {
+            if (area.Contains(areaPoint))
+                return true;
+        }
+        return false;
+    }
+    public void ShowAndHideAreas()
+    {
+        StopAllCoroutines(); // 중복 방지
+        StartCoroutine(ShowAndFadeCoroutine());
+    }
+
+    // 1초보여주고 2초간 천천히 감추기
+    private IEnumerator ShowAndFadeCoroutine()
+    {
+        foreach (var sr in visualizers)
+        {
+            sr.color = gizmoColor;
+            sr.enabled = true;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        float fadeDuration = 1f;
+        float elapsed = 0f;
+
+        while (elapsed < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(gizmoColor.a, 0f, elapsed / fadeDuration);
+
+            foreach (var sr in visualizers)
+            {
+                Color c = sr.color;
+                sr.color = new Color(c.r, c.g, c.b, alpha);
+            }
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        foreach (var sr in visualizers)
+        {
+            sr.enabled = false;
         }
     }
 }
