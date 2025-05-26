9d57922d66af840f9856e6970f3d4249a70c95d4
박진우 <tree6316mte@gmail.com>
2025-05-27 01:58:15 +0900

    [Fix] Castle HP 점등 오류 다시 수정

--- Assets/01.Scripts/01.UI/00.InGame/GaugeUI.cs
+++ Assets/01.Scripts/01.UI/00.InGame/GaugeUI.cs
@@ -61,10 +61,7 @@ public class GaugeUI : MonoBehaviour
         this.isImgFlash = isImgFlash;
         this.flashAction = flashAction;
 
-        if (!isImgFlash)
-        {
-            UpdateFill(0f);
-        }
+        fillImage.fillAmount = 0f;
     }
 
     // 반응형 프로퍼티의 값이 변경되면 실행할 함수. 값에 따라 이미지의 fillAmount가 바뀜
