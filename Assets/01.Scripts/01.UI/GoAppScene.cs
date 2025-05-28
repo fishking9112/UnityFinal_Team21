6e7ad1c8b82e7c6feb674f552ed17a3e5fb1eacb
dalsi <tkdwns8632@gmail.com>
2025-05-29 02:28:04 +0900

    [Feat] 인트로 씬 제작

---
+++ Assets/01.Scripts/01.UI/GoAppScene.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoAppScene : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene("AppScene");
+    }
+
+}
