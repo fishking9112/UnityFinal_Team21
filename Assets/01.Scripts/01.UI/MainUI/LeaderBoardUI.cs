e6aaa07307c4c5708cc25ea41aa6a151939da671
dalsi <tkdwns8632@gmail.com>
2025-05-28 21:43:34 +0900

    [Feat] 리더보드 새로고침 버튼 추가 및 도감 스크롤 속도 증가

--- Assets/01.Scripts/01.UI/MainUI/LeaderBoardUI.cs
+++ Assets/01.Scripts/01.UI/MainUI/LeaderBoardUI.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,40 +9,100 @@ public class LeaderBoardUI : MonoBehaviour
     [SerializeField] private RankPanel myRankPanel;
     [SerializeField] private RankPanel rankPanelPrefab;
     [SerializeField] private GameObject noRankText;
-    [SerializeField] private Button CloseBtn;
+    [SerializeField] private Button closeBtn;
+    [SerializeField] private Button refreshBtn;
+    [SerializeField] private Image refreshCoolTiemImage;
 
     private RankInfo myRankInfo;
     private List<RankInfo> rankerInfo;
 
+    private const float refreshCooldown = 10f; // 쿨타임 10초
+    private float cooldownTimer = 0f;
+    private bool isCooldownActive = false;
+
     private void Awake()
     {
         noRankText.SetActive(false);
-        CloseBtn.onClick.AddListener(() => gameObject.SetActive(false));
+        closeBtn.onClick.AddListener(() => gameObject.SetActive(false));
+        refreshBtn.onClick.AddListener(OnClickRefresh);
+        refreshCoolTiemImage.fillAmount = 0f;
+        refreshBtn.interactable = true;
     }
 
     private void OnEnable()
     {
-        rankerInfo = UGSManager.Instance.Leaderboard.rankerInfo;
-        myRankInfo = UGSManager.Instance.Leaderboard.myRankerInfo;
+        UpdateLeaderboardUI();
+        // 쿨타임 초기화
+        StartCooldown();
+    }
 
+    private void Update()
+    {
+        if (isCooldownActive)
+        {
+            cooldownTimer -= Time.deltaTime;
+            refreshCoolTiemImage.fillAmount = cooldownTimer / refreshCooldown;
+            Debug.Log("sfe");
+            if (cooldownTimer <= 0)
+            {
+                StopCooldown();
+            }
+        }
+    }
 
-        foreach (Transform child in rankScrollContent.transform)
+    private void OnClickRefresh()
+    {
+        if (isCooldownActive) return;
+
+        RefreshLeaderboardAsync().Forget();
+        StartCooldown();
+    }
+
+    private async UniTaskVoid RefreshLeaderboardAsync()
+    {
+        noRankText.SetActive(false);
+
+        // 서버에서 최신 데이터 불러오기
+        await UGSManager.Instance.LoadLeaderboardTop10Async();
+
+        // UI 갱신
+        UpdateLeaderboardUI();
+    }
+
+    private void UpdateLeaderboardUI()
+    {
+        foreach (Transform child in rankScrollContent)
         {
             Destroy(child.gameObject);
         }
 
-        if(rankerInfo.Count <= 0)
-        {
-            noRankText.SetActive(true);
-        }
+        rankerInfo = UGSManager.Instance.Leaderboard.rankerInfo;
+        myRankInfo = UGSManager.Instance.Leaderboard.myRankerInfo;
 
-        // 랭크는 10등까지만 표기
+        noRankText.SetActive(rankerInfo.Count <= 0);
+
         for (int i = 0; i < rankerInfo.Count; i++)
         {
-            RankPanel rankpanel = Instantiate(rankPanelPrefab, rankScrollContent);
-            rankpanel.SetRankPanel(rankerInfo[i]);
+            RankPanel rankPanel = Instantiate(rankPanelPrefab, rankScrollContent);
+            rankPanel.SetRankPanel(rankerInfo[i]);
         }
 
         myRankPanel.SetRankPanel(myRankInfo);
+    }
+
+    private void StartCooldown()
+    {
+        isCooldownActive = true;
+        cooldownTimer = refreshCooldown;
+        refreshBtn.interactable = false;
+        refreshCoolTiemImage.fillAmount = 1f;
+    }
+
+    private void StopCooldown()
+    {
+        isCooldownActive = false;
+        cooldownTimer = 0f;
+        refreshBtn.interactable = true;
+        refreshCoolTiemImage.fillAmount = 0f;
     }
 }
