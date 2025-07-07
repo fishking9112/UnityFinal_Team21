226d14fff79da9d3f64d560aa5612ba5ea3d2267
박진우 <tree6316mte@gmail.com>
2025-07-07 19:04:26 +0900

    [Feat] 업적 버튼 활성화

--- Assets/01.Scripts/01.UI/MainUI/TrophyPanel.cs
+++ Assets/01.Scripts/01.UI/MainUI/TrophyPanel.cs
@@ -8,22 +8,33 @@ using UnityEngine.Localization.Components;
 
 public class TrophyPanel : MonoBehaviour
 {
-    public int trophyId;
+    private TrophyInfo trophyInfo;
     public Image iconImg;
     public TextMeshProUGUI nameTxt;
     public LocalizeStringEvent nameLocalize;
     public LocalizeStringEvent descLocalize;
     public TextMeshProUGUI countTxt;
     public GameObject countGroup;
+    public GameObject completeGroup;
+    public Image rewardImg;
+    public TextMeshProUGUI rewardTxt;
+
     public Image checkImg;
     public Toggle toggle;
+    public Button rewardBtn;
+
 
     private bool isCount = false;
+
+    public void Awake()
+    {
+        rewardBtn.onClick.AddListener(() => { OnClickReward(); });
+    }
 
     // icon, 
     public void Init(int id, string iconName, string name, string desc, int countMax, int countCur, bool isActive, ToggleGroup toggleGroup)
     {
-        trophyId = id;
+        trophyInfo = DataManager.Instance.trophyDic[id];
 
         // icon선택
         if (iconName != null)
@@ -39,9 +50,17 @@ public class TrophyPanel : MonoBehaviour
         StringManager.Instance.SetString(desc, descLocalize);
         countTxt.text = $"{countCur} / {countMax}";
 
-        if (countMax <= countCur)
+        if (countMax <= countCur) // 완료 버튼 활성화
         {
             countGroup.SetActive(false);
+            if (TrophyManager.Instance.IsRewardTrophy(trophyInfo.id))
+            {
+                completeGroup.SetActive(true);
+            }
+            else
+            {
+                completeGroup.SetActive(false);
+            }
             isCount = true;
         }
         else
@@ -59,6 +78,18 @@ public class TrophyPanel : MonoBehaviour
             checkImg.gameObject.SetActive(false);
         }
 
+        if (trophyInfo.unLockID != 0) // 돈이 아닌 해금
+        {
+            // 나중에 해금되는 것 만들 때 사용될 것(열쇠 아이콘)
+            rewardImg.sprite = DataManager.Instance.iconAtlas.GetSprite("gameicon_tilemap-Sheet_2099");
+            rewardTxt.text = "";
+        }
+        else
+        {
+            // 골드 획득
+            rewardImg.sprite = DataManager.Instance.iconAtlas.GetSprite("gameicon_tilemap-Sheet_1179");
+            rewardTxt.text = trophyInfo.reward.ToString();
+        }
         toggle.group = toggleGroup;
     }
 
@@ -70,7 +101,8 @@ public class TrophyPanel : MonoBehaviour
             return;
         }
 
-        bool isClear = TrophyManager.Instance.GetRewardTrophy(trophyId);
-        if (isClear) countGroup.SetActive(false);
+        bool isClear = TrophyManager.Instance.GetRewardTrophy(trophyInfo.id, completeGroup.transform.position);
+        if (isClear) completeGroup.SetActive(true);
+
     }
 }
