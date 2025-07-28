d2a463ea3c4e5e6733f9ea7c7d779a5960430b09
dalsi <tkdwns8632@gmail.com>
2025-07-28 16:45:37 +0900

    [Fix] 골드 획득 테스트키 삭제

--- Assets/01.Scripts/00.Manager/GameManager.cs
+++ Assets/01.Scripts/00.Manager/GameManager.cs
@@ -70,12 +70,11 @@ public class GameManager : MonoSingleton<GameManager>
         if (Input.GetKeyDown(KeyCode.H))
         {
             castle.TakeDamaged(100f);
-        }*/
+        }
         if (Input.GetKeyDown(KeyCode.G))
         {
             AddGold(100);
         }
-        /*
         if (Input.GetKeyDown(KeyCode.S))
         {
             UGSManager.Instance.SaveLoad.SaveAsync().Forget();
