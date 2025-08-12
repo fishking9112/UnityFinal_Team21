8357c03bca125406531ba9ee8d930a1b22ded487
jj930220 <jj930220@gmail.com>
2025-08-12 18:01:15 +0900

    [Feat] 코드 정리

--- Assets/Editor/TestSceneAction.cs
+++ Assets/Editor/TestSceneAction.cs
@@ -4,7 +4,6 @@ using UnityEditor;
 using UnityEditor.Localization;
 using UnityEngine;
 using UnityEngine.Localization;
-using UnityEngine.SceneManagement;
 using UnityEngine.Localization.Tables;
 using UnityEngine.Localization.Settings;
 
@@ -14,23 +13,20 @@ public class TestSceneAction : EditorWindow
     private bool fold_enh;
     private bool fold_hero;
     private bool fold_kill;
-
     private bool fold_etc;
-    private bool isMortal=false;
-    private bool isInfinity=false;
-    private bool prevMortal=false;
-    private bool prevInfinity=false;
+
+    private bool isMortal = false;
+    private bool isInfinity = false;
 
     private GameHUD gameHud;
     private TestStart tStart;
-
-
     private Vector2 scroll = Vector2.zero;
 
     private Dictionary<int, int> heroWeapon = new Dictionary<int, int>();
 
-    private StringTableCollection stringTableCollection;
-    StringTable stringTable;
+    private StringTable stringTable;
+
+    private bool isTestMode; 
 
     [MenuItem("Window/TestScene")]
     public static void ShowEditor()
@@ -40,51 +36,55 @@ public class TestSceneAction : EditorWindow
 
     public void OnEnable()
     {
-
         isMortal = false;
         isInfinity = false;
-        prevMortal = false;
-        prevInfinity = false;
     }
 
     private void OnGUI()
     {
-        stringTable = GetFixedStringTable("ko");
-        scroll = GUILayout.BeginScrollView(scroll);
-
-        ShowAbility();
-        ShowEnhance();
-        ShowHeroSet();
-        KillAllUnit();
-
-        ShowETC();
-
-        GUILayout.EndScrollView();
-        if (Event.current.type == EventType.ScrollWheel)
+        if (!Application.isPlaying)
         {
-            scroll.y += Event.current.delta.y * 20; 
-            Repaint(); 
+            return;
         }
 
+
+        stringTable = GetFixedStringTable("ko");
+        isTestMode = GameManager.Instance?.isTest != false;
+
+        try
+        {
+            scroll = GUILayout.BeginScrollView(scroll);
+
+            ShowAbility();
+            ShowEnhance();
+            ShowHeroSet();
+            KillAllUnit();
+            ShowETC();
+        }
+        finally
+        {
+            GUILayout.EndScrollView();
+        }
+
+        if (Event.current.type == EventType.ScrollWheel)
+        {
+            scroll.y += Event.current.delta.y * 20;
+            Repaint();
+        }
     }
 
-    #region Func
+    #region UI Sections
+
     private void ShowAbility()
     {
         fold_abi = EditorGUILayout.Foldout(fold_abi, "QueenAbility");
 
-        //if (SceneManager.GetActiveScene().name != "TestScene")
-        if(GameManager.Instance.isTest==false)
-        {
-            fold_abi = false;
-            return;
-        }
-        if (fold_abi)
+        if (isTestMode && fold_abi)
         {
             EditorGUILayout.BeginVertical("box");
+
             foreach (var kvp in DataManager.Instance.queenAbilityDic)
             {
-
                 int currentValue = QueenAbilityUpgradeManager.Instance.GetLevel(kvp.Value.id);
                 string value = stringTable[kvp.Value.name].Value;
                 int maxLv = DataManager.Instance.queenAbilityDic[kvp.Value.id].maxLevel;
@@ -99,13 +99,14 @@ public class TestSceneAction : EditorWindow
                 QueenAbilityUpgradeManager.Instance.ApplyAllEffects();
                 MonsterManager.Instance.WaitUntilInitCompleteAndSetup().Forget();
             }
+
             EditorGUILayout.EndVertical();
         }
     }
 
     private void ResetStat()
     {
-        if(tStart==null)
+        if (tStart == null)
             tStart = GameObject.Find("StartTrigger").GetComponent<TestStart>();
         tStart.ResetAllMonsterStats();
     }
@@ -114,21 +115,12 @@ public class TestSceneAction : EditorWindow
     {
         fold_enh = EditorGUILayout.Foldout(fold_enh, "Enhance");
 
-        if (GameManager.Instance.isTest == false)
-        {
-            fold_enh = false;
-            return;
-        }    
-
-
-
-        if (fold_enh)
+        if (isTestMode && fold_enh)
         {
             EditorGUILayout.BeginVertical("box");
+
             if (gameHud == null)
-            {
                 gameHud = GameObject.Find("GameHUD(Clone)")?.GetComponent<GameHUD>();
-            }
 
             foreach (var key in DataManager.Instance.queenEnhanceDic)
             {
@@ -138,149 +130,99 @@ public class TestSceneAction : EditorWindow
                 currentValue = EditorGUILayout.IntField(value, currentValue);
                 if (GUILayout.Button("+"))
                 {
-                    
-                    QueenEnhanceInfo info = (DataManager.Instance.queenEnhanceDic[key.Value.id]);
-
+                    QueenEnhanceInfo info = DataManager.Instance.queenEnhanceDic[key.Value.id];
                     if (info.maxLevel > currentValue)
-                    {
                         gameHud.queenEnhanceUI.ApplyInhance(info);
-                    }
                 }
-
             }
+
             EditorGUILayout.EndVertical();
-
-
         }
     }
 
     private void ShowHeroSet()
     {
         fold_hero = EditorGUILayout.Foldout(fold_hero, "HeroSetting");
-        if (GameManager.Instance.isTest == false)
-        {
-            fold_hero = false;
-            return;
-        }
-        var a =DataManager.Instance.heroAbilityDic;
 
-        if(fold_hero)
+        if (isTestMode && fold_hero)
         {
             EditorGUILayout.BeginVertical("box");
 
             int sum = 0;
-            foreach(var ab in a)
+            foreach (var ab in DataManager.Instance.heroAbilityDic)
             {
-                if(!heroWeapon.ContainsKey(ab.Key))
-                {
+                if (!heroWeapon.ContainsKey(ab.Key))
                     heroWeapon.Add(ab.Key, 0);
-                }
+
                 string value = stringTable[ab.Value.name].Value;
 
                 heroWeapon[ab.Key] = (int)EditorGUILayout.Slider(value, heroWeapon[ab.Key], 0, 8);
                 sum += heroWeapon[ab.Key];
-                if(sum>30)
-                {
-                    heroWeapon[ab.Key] = (int)EditorGUILayout.Slider(ab.Key.ToString(), 0, 0, 8);
 
-                }
+                if (sum > 30)
+                    heroWeapon[ab.Key] = (int)EditorGUILayout.Slider(ab.Key.ToString(), 0, 0, 8);
             }
-            
+
             EditorGUILayout.IntField("레벨", sum);
             EditorGUILayout.HelpBox("최대레벨에 주의하세요, 현재 테이블상 최대레벨은 30입니다", MessageType.Warning);
-            if(GUILayout.Button("소환"))
+
+            if (GUILayout.Button("소환") && sum > 0)
             {
-                if (GameManager.Instance.isTest == false)
-                {
-
-                }
-                else if(sum==0)
-                {
-
-                }
-                else
-                {
-
-                    HeroStatusInfo statusInfo=HeroManager.Instance.SetTestHero(sum);
-                    HeroController hero = HeroPoolManager.Instance.GetObject(SpawnPointManager.Instance.heroPoint.GetRandomPosition());
-                    hero?.StatInit(statusInfo, HeroManager.Instance.isHealthUI,heroWeapon);
-                }
-
+                HeroStatusInfo statusInfo = HeroManager.Instance.SetTestHero(sum);
+                HeroController hero = HeroPoolManager.Instance.GetObject(SpawnPointManager.Instance.heroPoint.GetRandomPosition());
+                hero?.StatInit(statusInfo, HeroManager.Instance.isHealthUI, heroWeapon);
             }
-            EditorGUILayout.EndVertical();
 
+            EditorGUILayout.EndVertical();
         }
     }
 
-
     private void KillAllUnit()
     {
-
         fold_kill = EditorGUILayout.Foldout(fold_kill, "Kill");
 
-        if (GameManager.Instance.isTest == false)
-        {
-            fold_kill = false;
-            return;
-        }
-
-        if (fold_kill)
+        if (isTestMode && fold_kill)
         {
             EditorGUILayout.BeginVertical("box");
 
             if (GUILayout.Button("Kill All Hero"))
             {
                 foreach (var h in HeroManager.Instance.hero)
-                {
                     h.Value.Die();
-                }
             }
+
             EditorGUILayout.EndVertical();
         }
     }
 
     private void ShowETC()
     {
-        fold_etc= EditorGUILayout.Foldout(fold_etc, "ETC");
+        fold_etc = EditorGUILayout.Foldout(fold_etc, "ETC");
 
-        if(GameManager.Instance.isTest==false)
-        {
-            fold_etc = false;
-            return;
-        }
-
-        if(fold_etc)
+        if (isTestMode && fold_etc)
         {
             EditorGUILayout.BeginVertical("box");
 
-            isMortal = EditorGUILayout.Toggle("성채 무적", isMortal);
-            isInfinity = EditorGUILayout.Toggle("소환 무한", isInfinity);
+            bool newMortal = EditorGUILayout.Toggle("성채 무적", isMortal);
+            bool newInfinity = EditorGUILayout.Toggle("소환 무한", isInfinity);
 
-            if(prevMortal!=isMortal)
+            if (newMortal != isMortal)
             {
-                prevMortal = isMortal;
+                isMortal = newMortal;
                 GameManager.Instance.isMortal = isMortal;
             }
 
-            if(prevInfinity!=isInfinity)
+            if (newInfinity != isInfinity)
             {
-                prevInfinity = isInfinity;
-                GameManager.Instance.isInf=isInfinity;
+                isInfinity = newInfinity;
+                GameManager.Instance.isInf = isInfinity;
             }
+
             EditorGUILayout.EndVertical();
         }
-
     }
 
-
-
     #endregion
-
-
-
-
-
-
 
     private StringTable GetFixedStringTable(string localeCode)
     {
@@ -293,12 +235,8 @@ public class TestSceneAction : EditorWindow
         Locale fixedLocale = LocalizationSettings.AvailableLocales.GetLocale(localeCode);
 
         if (fixedLocale == null)
-        {
             return null;
-        }
 
         return (StringTable)collection.GetTable(fixedLocale.Identifier);
     }
-
-
 }
