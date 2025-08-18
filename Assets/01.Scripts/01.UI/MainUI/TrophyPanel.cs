248494c3fa7a2a253fb638e68243811f6be1e97c
dalsi <tkdwns8632@gmail.com>
2025-08-18 16:39:10 +0900

    [Feat] 단일 게임 업적은 00/00으로 표기하지않고 미달성으로 표기

--- Assets/01.Scripts/01.UI/MainUI/TrophyPanel.cs
+++ Assets/01.Scripts/01.UI/MainUI/TrophyPanel.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Localization.Components;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
 
 public class TrophyPanel : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class TrophyPanel : MonoBehaviour
     public LocalizeStringEvent nameLocalize;
     public LocalizeStringEvent descLocalize;
     public TextMeshProUGUI countTxt;
+    public GameObject nonStackGroup;
     public GameObject countGroup;
     public GameObject completeGroup;
     public Image rewardImg;
@@ -48,10 +50,22 @@ public class TrophyPanel : MonoBehaviour
         // nameTxt.text = name;
         StringManager.Instance.SetString(name, nameLocalize);
         StringManager.Instance.SetString(desc, descLocalize);
-        countTxt.text = $"{countCur} / {countMax}";
+
+        if (trophyInfo.type == TrophyType.Stack)
+        {
+            nonStackGroup.SetActive(false);
+            countGroup.SetActive(true);
+            countTxt.text = $"{countCur} / {countMax}";
+        }
+        else
+        {
+            nonStackGroup.SetActive(true);
+            countGroup.SetActive(false);
+        }
 
         if (countMax <= countCur) // 완료 버튼 활성화
         {
+            nonStackGroup.SetActive(false);
             countGroup.SetActive(false);
             if (TrophyManager.Instance.IsRewardTrophy(trophyInfo.id))
             {
