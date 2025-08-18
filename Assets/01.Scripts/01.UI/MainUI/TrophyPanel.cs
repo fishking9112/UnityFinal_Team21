876befe493134ff1329b1b32668705425893ebd0
jj930220 <jj930220@gmail.com>
2025-08-18 19:15:05 +0900

    [Feat] 어드레서블 빌드 및 에러코드 정리
    사용하지않는 네임스페이스에서 에러 발생해서 삭제

--- Assets/01.Scripts/01.UI/MainUI/TrophyPanel.cs
+++ Assets/01.Scripts/01.UI/MainUI/TrophyPanel.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Localization.Components;
-using UnityEditor.Localization.Plugins.XLIFF.V12;
 
 public class TrophyPanel : MonoBehaviour
 {
