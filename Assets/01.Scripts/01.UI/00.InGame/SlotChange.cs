0cd9cdfcd1a0ad2d9f1550b3a425e6622732831e
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-09-03 18:27:06 +0900

    [Fix] 슬롯 변경 로직 변경
    이전 : 탭 -> 연출 -> curSlot 변경 -> 다시 탭 가능
    현재 : 탭 -> curSlot 변경 -> 연출 -> 다시 탭 가능

--- Assets/01.Scripts/01.UI/00.InGame/SlotChange.cs
+++ Assets/01.Scripts/01.UI/00.InGame/SlotChange.cs
@@ -52,60 +52,101 @@ public class SlotChange : MonoBehaviour
     // Tab 키를 누르면 몬스터슬롯과 권능 슬롯이 변경됨
     public void OnChangeSlots(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Started)
+        if (context.phase != InputActionPhase.Started)
         {
-            if (isChange)
-            {
-                return;
-            }
-            if (StaticUIManager.Instance.hudLayer.GetHUD<GameHUD>().isPaused == true)
-            {
-                return;
-            }
-
-            if (queenActiveSkillGroupTransform == null || monsterGroupTransform == null)
-            {
-                Utils.LogWarning("RectTransform objects are null. Cannot perform slot change.");
-                return;
-            }
-
-            isChange = true;
-
-            Vector3 queenActiveSkillPos = queenActiveSkillGroupTransform.anchoredPosition;
-            Vector3 monsterPos = monsterGroupTransform.anchoredPosition;
-
-            Vector3[] queenActiveSkillPath = CreateArc(queenActiveSkillPos, monsterPos, controller.curSlot == QueenSlot.Monster ? arcHeight : -arcHeight);
-            Vector3[] monsterPath = CreateArc(monsterPos, queenActiveSkillPos, controller.curSlot == QueenSlot.Monster ? -arcHeight : arcHeight);
-
-            Sequence seq = DOTween.Sequence();
-
-            // 시작할 때
-            seq.Append(queenActiveSkillGroupTransform.DOLocalPath(queenActiveSkillPath, duration, PathType.CatmullRom).SetEase(Ease.InOutQuad))
-               .Join(monsterGroupTransform.DOLocalPath(monsterPath, duration, PathType.CatmullRom).SetEase(Ease.InOutQuad));
-
-            // 포물선 최고점에 이르렀을 때
-            seq.InsertCallback(duration / 2f, SetOrder);
-
-            // 끝날 때
-            seq.OnComplete(CheangeEnd);
+            return;
         }
+
+        if (isChange)
+        {
+            return;
+        }
+
+        if (StaticUIManager.Instance.hudLayer.GetHUD<GameHUD>().isPaused)
+        {
+            return;
+        }
+
+        if (queenActiveSkillGroupTransform == null || monsterGroupTransform == null)
+        {
+            return;
+        }
+
+        isChange = true;
+
+        // curSlot을 바로 바꿈
+        controller.curSlot = controller.curSlot == QueenSlot.Monster ? QueenSlot.QueenActiveSkill : QueenSlot.Monster;
+
+        if (controller.curSlot == QueenSlot.Monster)
+        {
+            saveMonster = controller.selectedMonsterId;
+        }
+        else
+        {
+            saveSkill = controller.selectedQueenActiveSkill;
+        }
+
+        if (controller.curSlot == QueenSlot.Monster)
+        {
+            controller.selectedMonsterId = saveMonster;
+            if (DataManager.Instance.monsterDic.TryGetValue(saveMonster, out var monsterData))
+            {
+                var icon = DataManager.Instance.iconAtlas.GetSprite(monsterData.icon);
+                controller.cursorIcon.GetComponent<SpriteRenderer>().sprite = icon;
+            }
+            else
+            {
+                controller.cursorIcon.GetComponent<SpriteRenderer>().sprite = null;
+            }
+
+            summonGauge.SetActive(true);
+            queenActiveSkillGauge.SetActive(false);
+            monsterSlotNumGroup.SetActive(true);
+            skillSlotNumGroup.SetActive(false);
+        }
+        else
+        {
+            controller.selectedQueenActiveSkill = saveSkill;
+            controller.cursorIcon.GetComponent<SpriteRenderer>().sprite = null;
+
+            summonGauge.SetActive(false);
+            queenActiveSkillGauge.SetActive(true);
+            monsterSlotNumGroup.SetActive(false);
+            skillSlotNumGroup.SetActive(true);
+        }
+
+        Vector3 queenActiveSkillPos = queenActiveSkillGroupTransform.anchoredPosition;
+        Vector3 monsterPos = monsterGroupTransform.anchoredPosition;
+
+        Vector3[] queenActiveSkillPath = CreateArc(
+            queenActiveSkillPos,
+            monsterPos,
+            controller.curSlot == QueenSlot.Monster ? arcHeight : -arcHeight
+        );
+        Vector3[] monsterPath = CreateArc(
+            monsterPos,
+            queenActiveSkillPos,
+            controller.curSlot == QueenSlot.Monster ? -arcHeight : arcHeight
+        );
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(queenActiveSkillGroupTransform.DOLocalPath(queenActiveSkillPath, duration, PathType.CatmullRom).SetEase(Ease.InOutQuad))
+           .Join(monsterGroupTransform.DOLocalPath(monsterPath, duration, PathType.CatmullRom).SetEase(Ease.InOutQuad));
+
+        // 포물선 최고점에 이르렀을 때 순서 변경
+        seq.InsertCallback(duration / 2f, SetOrder);
+
+        seq.OnComplete(() =>
+        {
+            isChange = false;
+        });
     }
 
     // 슬롯의 순서를 바꿈. 현재 선택된 슬롯이 아니면 반 투명해지면서 현재슬롯에 가려지도록 렌더링 순서 변경
     private void SetOrder()
     {
         if (controller.curSlot == QueenSlot.Monster)
-        {
-            monsterGroupTransform.SetAsFirstSibling();
-            panelTransform.SetAsFirstSibling();
-            summonGauge.SetActive(false);
-            queenActiveSkillGauge.SetActive(true);
-            monsterSlotNumGroup.SetActive(false);
-            skillSlotNumGroup.SetActive(true);
-            queenActiveSkillGroup.DOFade(1f, 0.2f);
-            monsterGroup.DOFade(0.5f, 0.2f);
-        }
-        else if (controller.curSlot == QueenSlot.QueenActiveSkill)
         {
             monsterGroupTransform.SetAsLastSibling();
             panelTransform.SetAsFirstSibling();
@@ -116,53 +157,17 @@ public class SlotChange : MonoBehaviour
             queenActiveSkillGroup.DOFade(0.5f, 0.2f);
             monsterGroup.DOFade(1f, 0.2f);
         }
-    }
-
-    // 슬롯 변경이 끝날 때 호출. 현재 슬롯의 상태를 바꿔줌
-    private void CheangeEnd()
-    {
-        // 슬롯 변경전 이전 값 저장
-        if (controller.curSlot == QueenSlot.Monster)
-        {
-            saveMonster = controller.selectedMonsterId;
-        }
         else
         {
-            saveSkill = controller.selectedQueenActiveSkill;
+            monsterGroupTransform.SetAsFirstSibling();
+            panelTransform.SetAsFirstSibling();
+            summonGauge.SetActive(false);
+            queenActiveSkillGauge.SetActive(true);
+            monsterSlotNumGroup.SetActive(false);
+            skillSlotNumGroup.SetActive(true);
+            queenActiveSkillGroup.DOFade(1f, 0.2f);
+            monsterGroup.DOFade(0.5f, 0.2f);
         }
-
-        // 슬롯 변경
-        controller.curSlot = controller.curSlot == QueenSlot.Monster ? QueenSlot.QueenActiveSkill : QueenSlot.Monster;
-
-        // 이전 값 불러오기
-        if (controller.curSlot == QueenSlot.Monster)
-        {
-            controller.selectedMonsterId = saveMonster;
-
-            if (DataManager.Instance.monsterDic.TryGetValue(saveMonster, out var monsterdata))
-            {
-                var icon = DataManager.Instance.iconAtlas.GetSprite(monsterdata.icon);
-                if (icon != null)
-                {
-                    controller.cursorIcon.GetComponent<SpriteRenderer>().sprite = icon;
-                }
-                else
-                {
-                    controller.cursorIcon.GetComponent<SpriteRenderer>().sprite = null;
-                }
-            }
-            else
-            {
-                controller.cursorIcon.GetComponent<SpriteRenderer>().sprite = null;
-            }
-        }
-        else if (controller.curSlot == QueenSlot.QueenActiveSkill)
-        {
-            controller.selectedQueenActiveSkill = saveSkill;
-            controller.cursorIcon.GetComponent<SpriteRenderer>().sprite = null;
-        }
-
-        isChange = false;
     }
 
     // 포물선을 만들어주는 함수
